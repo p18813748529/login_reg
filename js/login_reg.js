a3ce446e191dfc3389ed2b7215bea47542890edc
p18813748529 <18813748529@163.com>
2018-09-29 13:02:17 +0800

    增加输入框change时进行正则验证，将正则验证的用户名和密码验证分开

--- js/login_reg.js
+++ js/login_reg.js
@@ -18,13 +18,19 @@ var login_reg = (function(){
                     _this.ajaxCheck();
                 }
             };
+            this.$userInp.onchange = function(){
+                _this.checkUser();
+            };
+            this.$passInp.onchange = function(){
+                _this.checkPass();
+            };
         },
         // ajax检测用户是否存在，且密码是否正确
         ajaxCheck:function(){
             var _this = this;
             this.checkResult = true;
             function callBack(data){
-                type = JSON.parse(data).type;
+                var type = JSON.parse(data).type;
                 if(_this.type === "reg"){
                     if(type === "yes"){
                         var userParent = _this.$userInp.parentNode.parentNode;
@@ -34,25 +40,21 @@ var login_reg = (function(){
                     }
                     // 如果验证通过，跳转到用户页
                     if(_this.checkResult){
-                        if(_this.type === "reg"){
-                            function regCallBack(data){
-                                if(data.trim() === "true"){
-                                    alert("注册成功");
-                                    location.href = "manager.html?username=" + _this.$userInp.value;
-                                }
+                        function regCallBack(data){
+                            if(data.trim() === "true"){
+                                alert("注册成功");
+                                location.href = "http://localhost:8998/robot/qq.html?username=" + _this.$userInp.value;
                             }
-                            var regOptions = {
-                                method:"POST",
-                                data:{
-                                    username:_this.$userInp.value,
-                                    password:_this.$passInp.value
-                                },
-                                success:regCallBack
-                            };
-                            sendAjax("php/reg.php",regOptions);
-                        }else if(_this.type === "login"){
-                            location.href = "manager.html?username=" + _this.$userInp.value;
                         }
+                        var regOptions = {
+                            method:"POST",
+                            data:{
+                                username:_this.$userInp.value,
+                                password:_this.$passInp.value
+                            },
+                            success:regCallBack
+                        };
+                        sendAjax("php/reg.php",regOptions);
                     }
                 }else if(_this.type === "login"){
                     if(type === "user"){
@@ -68,10 +70,16 @@ var login_reg = (function(){
                     }
                     // 如果验证通过，跳转到用户页
                     if(_this.checkResult){
-                        location.href = "manager.html?username=" + _this.$userInp.value;
+                        location.href = "http://localhost:8998/robot/qq.html?username=" + _this.$userInp.value;
                     }
                 }
             }
+            // var callBack = null;
+            // if(this.type === "login"){
+            //     callBack = this.ajaxLogin;
+            // }else if(this.type === "reg"){
+            //     callBack = this.ajaxReg;
+            // }
             var options = {
                 method:"POST",
                 data:{
@@ -81,12 +89,63 @@ var login_reg = (function(){
                 success:callBack
             };
             sendAjax("php/login.php",options);
+        },ajaxLogin:(data) => {
+            console.log(this)
+            var type = JSON.parse(data).type;
+            if(type === "user"){
+                var userParent = this.$userInp.parentNode.parentNode;
+                userParent.classList.add("has-error");
+                this.$("label",userParent).innerText = "用户不存在";
+                this.checkResult = false;
+            }else if(type === "pass"){
+                var passParent = this.$passInp.parentNode.parentNode;
+                passParent.classList.add("has-error");
+                this.$("label",passParent).innerText = "密码错误";
+                this.checkResult = false;
+            }
+            // 如果验证通过，跳转到用户页
+            if(this.checkResult){
+                location.href = "http://localhost:8998/robot/qq.html?username=" + this.$userInp.value;
+            }
+        },
+        ajaxReg:(data) => {
+            var type = JSON.parse(data).type;
+            if(type === "yes"){
+                var userParent = this.$userInp.parentNode.parentNode;
+                userParent.classList.add("has-error");
+                this.$("label",userParent).innerText = "用户已存在";
+                this.checkResult = false;
+            }
+            // 如果验证通过，执行注册
+            if(this.checkResult){
+                function regCallBack(data){
+                    if(data.trim() === "true"){
+                        alert("注册成功");
+                        location.href = "http://localhost:8998/robot/qq.html?username=" + this.$userInp.value;
+                    }
+                }
+                var regOptions = {
+                    method:"POST",
+                    data:{
+                        username:this.$userInp.value,
+                        password:this.$passInp.value
+                    },
+                    success:regCallBack
+                };
+                sendAjax("php/reg.php",regOptions);
+            }
         },
         // 正则检测用户和密码格式是否正确
         regExpCheck:function(){
+            if(this.checkUser() && this.checkPass()){
+                return true;
+            }else{
+                return false;
+            }
+        },
+        checkUser:function(){
             var flag = true;
             var user = this.$userInp.value;
-            var pass = this.$passInp.value;
             var regUser = /^[0-9a-zA-Z]{5,13}$/;
             var userParent = this.$userInp.parentNode.parentNode;
             if(!regUser.test(user)){
@@ -97,6 +156,11 @@ var login_reg = (function(){
                 userParent.classList.remove("has-error");
                 this.$("label",userParent).innerText = "用户名";
             }
+            return flag;
+        },
+        checkPass:function(){
+            var flag = true;
+            var pass = this.$passInp.value;
             var regPass = /^[0-9a-zA-Z]{6,13}$/;
             var passParent = this.$passInp.parentNode.parentNode;
             if(!regPass.test(pass)){
